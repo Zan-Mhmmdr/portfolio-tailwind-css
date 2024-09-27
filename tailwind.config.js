f70babdfad0e9a7029d8eadd2b9aef8259ec8c14
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2024-09-27 15:28:06 +0700

    Portfolio Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320',
+      },
+    },
+  },
+  plugins: [],
+}
+
